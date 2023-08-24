293d5e59a9539d82b9e71c9679277267113fb1b5
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-08-24 20:39:00 +0000

    migrations issues

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     fav_planet = db.relationship('Fav_planet', backref='user', uselist=False)
     fav_people = db.relationship('Fav_people', backref='user', uselist=False)
 
